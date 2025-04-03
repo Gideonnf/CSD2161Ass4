634c172a07f3bfcc1f57c601628d8599299bf672
Rain <47514829+NivleMW@users.noreply.github.com>
2025-04-03 15:34:22 +0800

    lock bullets scoring to based of own bullets ig (not very good)

--- Game_Project/CSD1130_Asteroids/Src/GameState_Asteroids.cpp
+++ Game_Project/CSD1130_Asteroids/Src/GameState_Asteroids.cpp
@@ -407,21 +407,9 @@ void GameStateAsteroidsUpdate(void)
 			vel.y = BULLET_SPEED * sinf(gameData.spShip[gameData.currID]->dirCurr);
 			AEVec2Set(&scale, BULLET_SCALE_X, BULLET_SCALE_Y);
 			GameObjInst* bulletObj = bulletObjInstCreate(&pos, &vel, gameData.spShip[gameData.currID]->dirCurr);
-			unsigned int bulletID{};
-
-			for (unsigned long i = 0; i < GAME_OBJ_INST_NUM_MAX; i++)
-			{
-				GameObjInst* pInst = gameData.sGameObjInstList + i;
-				if (pInst == bulletObj)
-				{
-					bulletID = i;
-					break;
-				}
-			}
-
 			{
 				Packet pck(CMDID::BULLET_CREATED);
-				pck << gameData.currID << NetworkClient::Instance().GetTimeDiff() << bulletID << pos.x << pos.y << vel.x << vel.y << gameData.spShip[gameData.currID]->dirCurr;
+				pck << gameData.currID << NetworkClient::Instance().GetTimeDiff() << bulletObj->serverID << pos.x << pos.y << vel.x << vel.y << gameData.spShip[gameData.currID]->dirCurr;
 				NetworkClient::Instance().CreateMessage(pck);
 			}
 
@@ -728,6 +716,7 @@ GameObjInst* bulletObjInstCreate(AEVec2* pPos, AEVec2* pVel, float dir, uint32_t
 	if (id >= BULLET_ID_MAX)
 	{
 		pInst = gameData.sGameObjInstList + 4 + (gameData.currID * BULLET_ID_MAX) + gameData.bulletIDCount;
+		pInst->serverID = 4 + (gameData.currID * BULLET_ID_MAX) + gameData.bulletIDCount;
 		if (++gameData.bulletIDCount >= BULLET_ID_MAX)
 		{
 			gameData.bulletIDCount = 0;
@@ -735,7 +724,8 @@ GameObjInst* bulletObjInstCreate(AEVec2* pPos, AEVec2* pVel, float dir, uint32_t
 	}
 	else // valid ID
 	{
-		 pInst = gameData.sGameObjInstList + id;
+		pInst = gameData.sGameObjInstList + id;
+		pInst->serverID = id;
 	}
 	pInst->pObject = gameData.sGameObjList + TYPE_BULLET;
 	pInst->flag = FLAG_ACTIVE;
@@ -852,6 +842,8 @@ void CheckGOCollision()
 				switch (pInst_2->pObject->type)
 				{
 				case TYPE_BULLET:
+					if (pInst_2->serverID < 4 + (gameData.currID * BULLET_ID_MAX) || pInst_2->serverID > 4 + ((gameData.currID + 1) * BULLET_ID_MAX))
+						break;
 					if (CollisionIntersection_RectRect(pInst_1->boundingBox, pInst_1->velCurr, pInst_2->boundingBox, pInst_2->velCurr, firstTimeOfCollision))
 					{
 						// Destroy both the bullet and asteroid after
